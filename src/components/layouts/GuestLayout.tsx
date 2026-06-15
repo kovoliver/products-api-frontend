cf2bc63fc36dcb111174323ab4cd80cada380a78
kovoliver <kovacs.oliver1989@gmail.com>
2026-06-15 18:07:11 +0200

    GuestsLayout navigates to profile when isAuthenticated

--- src/components/layouts/GuestLayout.tsx
+++ src/components/layouts/GuestLayout.tsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import GlobalNotifications from "../overlays/GlobalNotifications";
 import GlobalConfirmation from "../overlays/GlobalConfirmations";
+import { useUserStore } from "../../core/stores/userStore";
 
 export default function GuestLayout() {
+    const isAuthenticated = useUserStore((state) => state.isAuthenticated);
+    const user = useUserStore((state) => state.user);
+
+    if(isAuthenticated && user) {
+        <Navigate to="/user/profile"/>
+    }
+
     return (
         <div className="max-w-6xl mx-auto rounded-md bg-amber-50 border border-[color-mix(in_oklch,var(--color-accent),black_5%)] p-10">
             <GlobalNotifications/>
